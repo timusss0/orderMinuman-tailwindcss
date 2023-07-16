724501816ddad868e8de6f9c70182dfcd7ced815
timusss0 <mustikavia86@gmail.com>
2023-07-16 14:31:07 +0700

    selesai membuat card

--- index.php
+++ index.php
@@ -11,11 +11,56 @@
   Silahkan Order minuman
   </h1>
 
-  <div class="bg-white rounded-t-lg shadow-md p-4 w-80">
-      <img src="img/drink1.jpeg" class="w-full rounded-lg" alt="">
-      <h2 class="text-xl font-semibold mb-2">Americano</h2>
+<div class="flex justify-center mb-5 p-5">
+  <div class="bg-white rounded-t-lg shadow-2xl p-4 w-80 md:w-50 lg:w-100 rounded-lg">
+      <img src="img/drink1.jpeg" class="w-full rounded-lg m-1" alt="">
+      <h2 class="text-xl font-semibold p-1">Americano</h2>
+      <p class="text-gray-600 mt-2 p-1 "> Lorem ipsum dolor sit amet.</p>
+      <button onclick="order('Americano :Rp 45.000' , 45000 , pesan)" class="bg-indigo-500 rounded-md p-2  text-white" >Pilih Minuman</button>
+  </div>
+
+  <div class="bg-white rounded-t-lg shadow-2xl p-4 m-5 w-80 md:w-50 lg:w-100 rounded-lg">
+      <img src="img/drink4.jpg" class="w-full rounded-lg m-1 " alt="">
+      <h2 class="text-xl font-semibold m-1">Americano</h2>
       <p class="text-gray-600 p-1">Lorem ipsum dolor sit amet.</p>
       <button onclick="order('Americano :Rp 45.000' , 45000 , pesan)" class="bg-indigo-500 rounded-md p-2  text-white" >Pilih Minuman</button>
   </div>
+
+
+  <div class="bg-white rounded-t-lg shadow-2xl p-4 w-80 md:w-50 lg:w-100 rounded-lg">
+      <img src="img/drink5.jpg" class="w-full rounded-lg m-1" alt="">
+      <h2 class="text-xl font-semibold m-1">Americano</h2>
+      <p class="text-gray-600 mt-2 p-1"> Lorem ipsum dolor sit amet.</p>
+      <button onclick="order('Americano :Rp 45.000' , 45000 , pesan)" class="bg-indigo-500 rounded-md p-2 text-white" >Pilih Minuman</button>
+  </div>
+</div>
+
+
+<div class="flex justify-center mb-5 p-5">
+  <div class="bg-white rounded-t-lg shadow-2xl p-4 w-80 md:w-50 lg:w-100 rounded-lg">
+      <img src="img/drink5.jpg" class="w-full rounded-lg m-1" alt="">
+      <h2 class="text-xl font-semibold p-1">Americano</h2>
+      <p class="text-gray-600 mt-2 p-1 "> Lorem ipsum dolor sit amet.</p>
+      <button onclick="order('Americano :Rp 45.000' , 45000 , pesan)" class="bg-indigo-500 rounded-md p-2  text-white" >Pilih Minuman</button>
+  </div>
+
+  <div class="bg-white rounded-t-lg shadow-2xl p-4 m-5 w-80 md:w-50 lg:w-100 rounded-lg">
+      <img src="img/drink6.jpg" class="w-full rounded-lg m-1 " alt="">
+      <h2 class="text-xl font-semibold m-1">Americano</h2>
+      <p class="text-gray-600 p-1">Lorem ipsum dolor sit amet.</p>
+      <button onclick="order('Americano :Rp 45.000' , 45000 , pesan)" class="bg-indigo-500 rounded-md p-2  text-white" >Pilih Minuman</button>
+  </div>
+
+
+  <div class="bg-white rounded-t-lg shadow-2xl p-4 w-80 md:w-50 lg:w-100 rounded-lg">
+      <img src="img/drink4.jpg" class="w-full rounded-lg m-1" alt="">
+      <h2 class="text-xl font-semibold m-1">Americano</h2>
+      <p class="text-gray-600 mt-2 p-1"> Lorem ipsum dolor sit amet.</p>
+      <button onclick="order('Americano :Rp 45.000' , 45000 , pesan)" class="bg-indigo-500 rounded-md p-2 text-white" >Pilih Minuman</button>
+  </div>
+</div>
+
+
+
 </body>
 </html>
